5dfe9a7bda91f496fbe64811770b19be4e34cd47
MiracleOverfl0w <fiw.contact.work@gmail.com>
2025-01-12 06:25:02 +0000

    first commit

---
+++ src/libs/libgfx.hpp
@@ -0,0 +1,8 @@
+#ifndef HIGHGUI_H
+	#define HIGHGUI_H
+	#include<opencv2/highgui.hpp>
+	
+#endif
+void Linktac(const char);
+
+void endgfx(cv::Mat*);
---
+++ src/main.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+#include"libs/libgfx.hpp"
+#ifndef IMSHOW_CPP
+	#define IMSHOW_CPP
+#endif
+int main(int argc,char* argv[]){
+	Linktac(argv[0]);
+}
