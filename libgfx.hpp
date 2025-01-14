c722afc7ab0a9a453e2cc3dc20088ab74423844e
MiracleOverfl0w <fiw.contact.work@gmail.com>
2025-01-14 16:38:52 +0700

    Delete libgfx.hpp

--- libgfx.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef HIGHGUI_H
-	#define HIGHGUI_H
-	#include<opencv2/highgui.hpp>
-	
-#endif
-#define __INSTALL__COMMAND__ "OpenGFXInstall"
-#define __INSTALL__COMMAND__LINUX__  "echo 'export PATH=~/OpenGFX:$PATH' >> ~/.bashrc"
-void Linktac(const char);
-void endgfx(cv::Mat*);
